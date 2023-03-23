29b85bf8abb9b57c0caf89cf821081b4a2f83957
гк <@adamant.local>
2023-03-23 19:04:19 +0300

    first_commit

---
+++ src/main/java/by/home/vitamin/VitaminApplication.java
@@ -0,0 +1,13 @@
+package by.home.vitamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaminApplication.class, args);
+	}
+
+}
